0a4d2e13dc3bba0d95bc9a89b1e0aab437381bbb
sangeethkc <keyceesangi@gmail.com>
2022-09-12 01:11:38 +0530

    Bar chart completed

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Legend,
+  ColumnSeries,
+  DataLabel,
+  Inject,
+  Category,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import { Header } from "../../components";
+import {
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
+
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Bar = () => {
-  return (
-    <div>Bar</div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Bar
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Bar" title="Bar Chart" />
+      <ChartComponent
+        id="charts"
+        height="420px"
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "33373E" : ""}
+      >
+        <Inject services={[ColumnSeries, Legend, Category, Tooltip, DataLabel]} />
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
+};
+
+export default Bar;
