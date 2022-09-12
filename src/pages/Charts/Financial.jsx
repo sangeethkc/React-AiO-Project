3e3283310743c4732bb0a68f7b5df03044d39b50
sangeethkc <keyceesangi@gmail.com>
2022-09-12 11:03:34 +0530

    Financial chart completed

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  HiloSeries,
+  DateTime,
+  Logarithmic,
+  Crosshair,
+  Zoom,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const Financial = () => {
-  return (
-    <div>Financial</div>
-  )
+import { useStateContext } from "../../contexts/ContextProvider";
+
+import {
+  financialChartData,
+  FinancialPrimaryXAxis,
+  FinancialPrimaryYAxis,
+} from "../../data/dummy";
+
+import { Header } from "../../components";
+
+const Date1 = new Date(2017, 1, 1);
+
+function filterValue(value) {
+  if (value.x >= Date1) {
+    // eslint-disable-next-line no-sequences
+    return value.x, value.high, value.low;
+  }
 }
 
-export default Financial
+const returnValue = financialChartData.filter(filterValue);
+
+const Financial = () => {
+  const { currentMode } = useStateContext();
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="financial" title="Financial" />
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          height="420px"
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          crosshair={{ enable: true, lineType: "Vertical", line: { width: 0 } }}
+          background={currentMode === "Dark" ? "33373E" : ""}
+        >
+          <Inject
+            services={[
+              HiloSeries,
+              DateTime,
+              Logarithmic,
+              Crosshair,
+              Zoom,
+              Tooltip,
+            ]}
+          />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={returnValue}
+              xName="x"
+              yName="low"
+              name="Apple Inc"
+              type="Hilo"
+              low="low"
+              high="high"
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
+};
+
+export default Financial;
